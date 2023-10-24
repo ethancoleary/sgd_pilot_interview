263a1a9b2626a69e6d92f2ec9fe3ff4356635c6a
ethanoleary <ethan.oleary@nhh.no>
2023-10-24 09:41:45 +0200

    commit full directory

--- intro/__init__.py
+++ intro/__init__.py
@@ -22,6 +22,9 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     optionA= models.BooleanField()
+    stage_payoff = models.CurrencyField()
+
+
 
 
 # PAGES
@@ -38,16 +41,45 @@ class RiskPref(Page):
     form_fields = ['optionA']
 
     def vars_for_template(player):
-        round_number = player.round_number
-        probability_1 = round_number
-        probability_2 = 10 - round_number
+        p1 = player.round_number
+        p2 = 10 - player.round_number
+        return {
+            'p1':p1,
+            'p2':p2
+        }
 
 
 class Results(Page):
 
     def is_displayed(player):
-        return player.round_number > 10
+        return player.round_number >= 10
 
+    def vars_for_template(player):
+
+        import random
+        roundnum = random.randint(1,10)
+        paying_round = player.in_round(roundnum)
+        pr_decision = paying_round.optionA
+        die_roll = random.randint(1, 10)
+
+        if pr_decision == 1 :
+
+            if roundnum < die_roll+1 :
+                player.stage_payoff = 2
+            else:
+                player.stage_payoff = 1.60
+
+        if pr_decision == 0:
+
+            if roundnum < die_roll+1 :
+                player.stage_payoff = 3.85
+            else:
+                player.stage_payoff = 0.10
+
+        return {
+            'pr_decision':pr_decision,
+            'roundnum':roundnum
+        }
 
 
 
